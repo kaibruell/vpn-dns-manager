60927edc92d6b6241bf79411114fb00589dcaf5f
kaibruell <kaibruell@gmx.de>
2025-11-27 13:27:03 +0100

    Adding the coredns Dockerfile to docker-compose 'compose.yml' file. Adding the 'CoreDNSLauncher.jar' Copying this file inside the coredns container Switching Container Image from coredns to 'ARG BASE=gcr.io/distroless/java21-nonroot' to have the JRE available. Integrading the Gradle Build for 'CoreDNSLauncher.java' inside the ./build.sh script and copying the .jar binary inside the docker container.
    	modified:   .gitignore
    	modified:   build.sh
    	modified:   compose.yml
    	modified:   container-modules/coredns
    	new file:   java/container-modules/coredns/.gitattributes
    	new file:   java/container-modules/coredns/.gitignore
    	new file:   java/container-modules/coredns/build.gradle
    	new file:   java/container-modules/coredns/gradle/libs.versions.toml
    	new file:   java/container-modules/coredns/gradle/wrapper/gradle-wrapper.jar
    	new file:   java/container-modules/coredns/gradle/wrapper/gradle-wrapper.properties
    	new file:   java/container-modules/coredns/gradlew
    	new file:   java/container-modules/coredns/gradlew.bat
    	new file:   java/container-modules/coredns/src/main/java/org/coredns/container/CoreDNSLauncher.java
    	new file:   java/container-modules/wireguard/bin/main/org/wireguard/container/WireGuardLauncher.class

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 
 root_dir=$(pwd)
 wireguard_lib_dir="$root_dir/container-modules/docker-wireguard/lib"
-
+coredns_plugin_java_dir="$root_dir/container-modules/coredns/plugin/ipblocker/java"
 
 # Build the 'wireguard-launcher.jar' and move it to wireguard_lib_dir
 cd java/container-modules/wireguard/
@@ -11,6 +11,13 @@ cd java/container-modules/wireguard/
 cd build/libs/
 mkdir -p $wireguard_lib_dir
 cp -r wireguard-launcher.jar "$wireguard_lib_dir"
+cd $root_dir
+
+cd java/container-modules/coredns/
+./gradlew build
+cd build/libs/
+mkdir -p "$coredns_plugin_java_dir"
+cp -r coredns-launcher.jar "$coredns_plugin_java_dir"
 
 cd $root_dir
 docker compose -f compose.yml build
---
+++ java/container-modules/coredns/src/main/java/org/coredns/container/CoreDNSLauncher.java
@@ -0,0 +1,7 @@
+package org.coredns.container;
+
+public class CoreDNSLauncher {
+    public static void main(String[] args) {
+	System.out.println("Hello World From CoreDNS Container Debian!");
+    }
+}
